e1c9bf202259d84db6ee2c8e70c52c23b02984bc
Matthias Bläsing <mblaesing@doppel-helix.eu>
2019-10-19 19:35:37 +0200

    Add license headers

--- src/main/java/eu/doppel_helix/jna/tlbcodegenerator/imp/TlbCoClass.java
+++ src/main/java/eu/doppel_helix/jna/tlbcodegenerator/imp/TlbCoClass.java
@@ -1,3 +1,27 @@
+/* Copyright (c) 2016 Matthias Bläsing, All Rights Reserved
+ *
+ * The contents of this file is dual-licensed under 2
+ * alternative Open Source/Free licenses: LGPL 2.1 or later and
+ * Apache License 2.0. (starting with JNA version 4.0.0).
+ *
+ * You can freely decide which license you want to apply to
+ * the project.
+ *
+ * You may obtain a copy of the LGPL License at:
+ *
+ * http://www.gnu.org/licenses/licenses.html
+ *
+ * A copy is also included in the downloadable source code package
+ * containing JNA, in file "LGPL2.1".
+ *
+ * You may obtain a copy of the Apache License at:
+ *
+ * http://www.apache.org/licenses/
+ *
+ * A copy is also included in the downloadable source code package
+ * containing JNA, in file "AL2.0".
+ */
+
 package eu.doppel_helix.jna.tlbcodegenerator.imp;
 
 import com.sun.jna.platform.win32.COM.ITypeInfo;
@@ -5,9 +29,7 @@ import com.sun.jna.platform.win32.COM.TypeInfoUtil;
 import com.sun.jna.platform.win32.OaIdl;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class TlbCoClass extends TlbEntry {
     private static final int IMPLTYPEFLAG_FDEFAULT = 0x00000001;
--- src/main/resources/eu/doppel_helix/jna/tlbcodegenerator/output/Interface.ftl
+++ src/main/resources/eu/doppel_helix/jna/tlbcodegenerator/output/Interface.ftl
@@ -1,4 +1,28 @@
 [#ftl]
+[#--
+Copyright (c) 2016 Matthias Bläsing, All Rights Reserved
+
+The contents of this file is dual-licensed under 2
+alternative Open Source/Free licenses: LGPL 2.1 or later and
+Apache License 2.0. (starting with JNA version 4.0.0).
+
+You can freely decide which license you want to apply to
+the project.
+
+You may obtain a copy of the LGPL License at:
+
+http://www.gnu.org/licenses/licenses.html
+
+A copy is also included in the downloadable source code package
+containing JNA, in file "LGPL2.1".
+
+You may obtain a copy of the Apache License at:
+
+http://www.apache.org/licenses/
+
+A copy is also included in the downloadable source code package
+containing JNA, in file "AL2.0".
+--]
 [#macro paramList params][#list params as param]
 [#if param?index > 0]            [/#if][#if (param.out)]VARIANT[#else]${typeLib.mapPrimitiveIfExists(param.javaType)} /* ${typeLib.mapPrimitiveIfExists(param.type)} */[/#if] ${param.name}[#sep], [/#sep][/#list][/#macro]
 
